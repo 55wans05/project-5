583df902f9cbcc3e64e77f9baf561c8ea579d5a3
Stephen Swanson <stephen.swanson@mailbox.org>
2023-11-14 15:38:05 -0800

    Save and load features

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -9,6 +9,9 @@ from flask import request
 import arrow  # Replacement for datetime, based on moment.js
 import acp_times  # Brevet time calculations
 import config
+import os
+import json
+from pymongo import MongoClient
 
 import logging
 
@@ -17,6 +20,9 @@ import logging
 ###
 app = flask.Flask(__name__)
 CONFIG = config.configuration()
+
+client = MongoClient("mongodb://" + os.environ["DB_HOSTNAME"], 27017)
+db = client.mydb
 
 ###
 # Pages
@@ -27,13 +33,13 @@ CONFIG = config.configuration()
 @app.route("/index")
 def index():
     app.logger.debug("Main page entry")
-    return flask.render_template('calc.html')
+    return flask.render_template("calc.html")
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     app.logger.debug("Page not found")
-    return flask.render_template('404.html'), 404
+    return flask.render_template("404.html"), 404
 
 
 ###############
@@ -50,15 +56,52 @@ def _calc_times():
     Expects one URL-encoded argument, the number of miles.
     """
     app.logger.debug("Got a JSON request")
-    km = request.args.get('km', 999, type=float)
-    dist = request.args.get('dist', 200, type=int)
-    begin = request.args.get('begin', 200, type=str)
+    km = request.args.get("km", 999, type=float)
+    dist = request.args.get("dist", 200, type=int)
+    begin = request.args.get("begin", 200, type=str)
     app.logger.debug("km={}".format(km))
     app.logger.debug("request.args: {}".format(request.args))
-    open_time = acp_times.open_time(km, dist, arrow.get(begin)).format('YYYY-MM-DDTHH:mm')
-    close_time = acp_times.close_time(km, dist, arrow.get(begin)).format('YYYY-MM-DDTHH:mm')
+    open_time = acp_times.open_time(km, dist, arrow.get(begin)).format(
+        "YYYY-MM-DDTHH:mm"
+    )
+    close_time = acp_times.close_time(km, dist, arrow.get(begin)).format(
+        "YYYY-MM-DDTHH:mm"
+    )
     result = {"open": open_time, "close": close_time}
     return flask.jsonify(result=result)
+
+
+@app.route("/get-calc")
+def retrieve_brevet():
+    # Inspired by https://stackoverflow.com/questions/62295223/pymongo-query-to-get-the-latest-document-from-the-collection-in-mongodb-using-py
+    brevet = db.brevets.find().sort("_id", -1).limit(1)
+
+    for i in brevet:
+        return flask.jsonify(
+            begin_date=i["begin_date"],
+            brevet_distance=i["brevet_distance"],
+            items=i["items"],
+        )
+
+    return json.dumps({"success": False}), 404, {"ContentType": "application/json"}
+
+
+@app.route("/set-calc", methods=["POST"])
+def set_brevet():
+    data = request.get_json()
+
+    # I'm being explicit about the data structure here for ease of reading
+
+    data = {
+        "begin_date": data["begin_date"],
+        "brevet_distance": data["brevet_distance"],
+        "items": data["items"],
+    }
+
+    db.brevets.insert_one(data)
+
+    # https://stackoverflow.com/questions/26079754/flask-how-to-return-a-success-status-code-for-ajax-call
+    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
 
 
 #############
